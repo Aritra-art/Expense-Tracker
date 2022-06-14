a861a1e9971e212417ef0acb8d10a2e4185b58f2
aritra <aritrachowdhury65@gmail.com>
2022-06-14 12:56:22 +0530

    onClick Event

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,10 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked !!");
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
@@ -8,6 +12,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 }
