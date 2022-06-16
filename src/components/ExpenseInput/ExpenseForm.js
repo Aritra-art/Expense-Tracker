a03a72471fb1274e3bebe9ee1ea90ddaef1c4652
aritra <aritrachowdhury65@gmail.com>
2022-06-16 13:26:54 +0530

    Multiple State

--- src/components/ExpenseInput/ExpenseForm.js
+++ src/components/ExpenseInput/ExpenseForm.js
@@ -1,14 +1,19 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangehandler = (event) => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
   };
   const amountChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredAmount(event.target.value);
   };
   const dateChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredDate(event.target.value);
   };
   return (
     <form>
