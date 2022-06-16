3335175c82adebddf91eb95ff62c27d70dc6285e
aritra <aritrachowdhury65@gmail.com>
2022-06-16 12:51:32 +0530

    onChange

--- src/components/ExpenseInput/ExpenseForm.js
+++ src/components/ExpenseInput/ExpenseForm.js
@@ -1,20 +1,39 @@
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const titleChangehandler = (event) => {
+    console.log(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="">Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangehandler} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Date</label>
-          <input type="date" min="2020-03-01" max="2025-03-02" />
+          <input
+            type="date"
+            min="2020-03-01"
+            max="2025-03-02"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
